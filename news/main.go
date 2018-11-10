cd99ffd77b3b3b9e5a7d0f7e2db330d72bc71787
Toshifumi Anan <toshifumi.anan@uzabase.com>
2018-11-10 18:24:29 +0900

    add news page

--- news/main.go
+++ news/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type NewsData struct {
+	PageTitle    string
+	NewsContents []NewsContent
+}
+
+type NewsContent struct {
+	Title       string
+	Description string
+}
+
+func newsHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("template/top.html"))
+	data := NewsData{
+		PageTitle: "top page",
+		NewsContents: []NewsContent{
+			{Title: "istio v1.0.0 release!!", Description: "this is a description statement"},
+			{Title: "istio v1.0.1 release!!", Description: "this is a description statement"},
+			{Title: "istio v1.0.2 release!!", Description: "this is a description statement"},
+			{Title: "istio v1.0.3 release!!", Description: "this is a description statement"},
+		},
+	}
+	t.Execute(w, data)
+}
 
 func main() {
-	fmt.Println()
+	http.HandleFunc("/news", newsHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
